b502f135821dce4dbf03bb9d0d99dfe7869cef27
tulusdeveloper <46865046+tulusdeveloper@users.noreply.github.com>
2019-01-25 14:43:00 +0300

    Add files via upload

---
+++ appointment.php
@@ -0,0 +1,24 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<title></title>
+		<a href="index.php">back to Home_Page</a>
+	</head>
+	<body>
+		<div><a href="./patient/login.php" <div class="container_locate"><input type="submit" class="btn btn-primary" name="pat_submit" value="Login as a patient" style="float:right;" </div></a></div><hr>
+		<div><a href="./doctor/login.php" <div class="container_locate"><input type="submit" class="btn btn-primary" name="pat_submit" value="Login as a doctor" style="float:right;" </div></a></div><hr>
+
+		<div class="jumbotron" style="background: url('images/hospital03.jpg')no-repeat;background-size: cover;
+		height: 300px;"></div>
+		
+
+		
+		
+
+
+
+     <script  src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>HOSPITAL YETU</title>
+<link href="css/multiColumnTemplate.css" rel="stylesheet" type="text/css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+  <header>
+    <img src="images/hospital03.jpg" width="100%" height="300px" alt=""/>
+    <div class="primary_header">
+      <h1 class="title"> HOSIWEB</h1>
+    </div>
+    <nav class="secondary_header" id="menu">
+      <ul>
+		  <li><a href="about.php">ABOUT US</a></li>
+        <li><a href="agency.php">DEPARTMENT</a></li>
+        <li><a href="page.php">EVENTS</a></li>
+        <li><a href="news.php">NEWS</a></li>
+        <li><a href="appointment.php">APPOINTMENT</a></li>
+        <li><a href="portfolio.php">CONTACT US</a></li>
+      </ul>
+    </nav>
+  </header>
+  <section>
+    <h2 class="noDisplay">Modern Hospital network</h2>
+    <article class="left_article">
+      <h3>content heading</h3>
+      <p>Medical research is constantly pushing the boundaries of health care and redefining what is and isn’t possible. Keeping pace with this vigorous progress, hospitals across the globe are continually updating their equipment and services in order to offer the latest in high-tech medical care. From specialist cancer centers with state-of-the-art digital imaging scanners, to multi-disciplinary mega-hospitals featuring cutting-edge medical robots.</p>
+      <p>THe modern hospital management system web is based on the global positioning of  network of health centers within a wide accessible locations.</p>
+    </article>
+  <aside class="right_article"><img src="images/hospital01.jpg" alt="" width="400" height="200" class="placeholder"/> </aside>
+  </section>
+  <div class="row">
+    <div class="columns">
+      <p class="thumbnail_align"> <img src="images/1.jpeg" alt="" class="thumbnail"/> </p>
+      <h4>TITLE</h4>
+      <p>Medical research is constantly pushing the boundaries of health care and redefining what is and isn’t possible. Keeping pace with this vigorous progress, hospitals across the globe are continually updating their equipment and services in order to offer the latest in high-tech medical care. From specialist cancer centers with state-of-the-art digital imaging scanners, to multi-disciplinary mega-hospitals featuring cutting-edge medical robots.</p>
+    </div>
+    <div class="columns">
+      <p class="thumbnail_align"> <img src="images/4.jpeg" alt="" class="thumbnail"/> </p>
+      <h4>TITLE</h4>
+      <p>Medical research is constantly pushing the boundaries of health care and redefining what is and isn’t possible. Keeping pace with this vigorous progress, hospitals across the globe are continually updating their equipment and services in order to offer the latest in high-tech medical care. From specialist cancer centers with state-of-the-art digital imaging scanners, to multi-disciplinary mega-hospitals featuring cutting-edge medical robots.</p>
+    </div>
+    <div class="columns">
+      <p class="thumbnail_align"> <img src="images/4.jpeg" alt="" class="thumbnail"/> </p>
+      <h4>TITLE</h4>
+      <p>Medical research is constantly pushing the boundaries of health care and redefining what is and isn’t possible. Keeping pace with this vigorous progress, hospitals across the globe are continually updating their equipment and services in order to offer the latest in high-tech medical care. From specialist cancer centers with state-of-the-art digital imaging scanners, to multi-disciplinary mega-hospitals featuring cutting-edge medical robots.</p>
+    </div>
+    <div class="columns">
+      <p class="thumbnail_align"> <img src="images/4.jpeg" alt="" class="thumbnail"/> </p>
+      <h4>TITLE</h4>
+      <p>Medical research is constantly pushing the boundaries of health care and redefining what is and isn’t possible. Keeping pace with this vigorous progress, hospitals across the globe are continually updating their equipment and services in order to offer the latest in high-tech medical care. From specialist cancer centers with state-of-the-art digital imaging scanners, to multi-disciplinary mega-hospitals featuring cutting-edge medical robots.</p>
+    </div>
+  </div>
+  <div class="row blockDisplay">
+    <div class="column_half left_half">
+      <h2 class="column_title">OUR MISSION</h2>
+      <p>Medical research is constantly pushing the boundaries of health care and redefining what is and isn’t possible. Keeping pace with this vigorous progress, hospitals across the globe are continually updating their equipment and services in order to offer the latest in high-tech medical care. From specialist cancer centers with state-of-the-art digital imaging scanners, to multi-disciplinary mega-hospitals featuring cutting-edge medical robots.</p>
+    </div>
+    <div class="column_half right_half">
+      <h2 class="column_title">OUR VISSION</h2>
+      <p>Medical research is constantly pushing the boundaries of health care and redefining what is and isn’t possible. Keeping pace with this vigorous progress, hospitals across the globe are continually updating their equipment and services in order to offer the latest in high-tech medical care. From specialist cancer centers with state-of-the-art digital imaging scanners, to multi-disciplinary mega-hospitals featuring cutting-edge medical robots.</p>
+    </div>
+  </div><br><br>
+  <div class="social">
+    <p class="social_icon"><img src="images/bkg_06.jpg" width="100" alt="" class="thumbnail"/></p>
+    <p class="social_icon"><img src="images/bkg_06.jpg" width="100" alt="" class="thumbnail"/></p>
+    <p class="social_icon"><img src="images/bkg_06.jpg" width="100" alt="" class="thumbnail"/></p>
+    <p class="social_icon"><img src="images/bkg_06.jpg" width="100" alt="" class="thumbnail"/></p>
+  </div>
+  <footer class="secondary_header footer">
+    <div class="copyright">&copy;2019 - <strong>SIMPLE Theme</strong></div>
+  </footer>
+</div>
+</body>
+</html>
